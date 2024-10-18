c4d5948dc0f7492a7708960b50149639ccc7b732
Bastien-Allemand <ballemandleungpin@gaming.tech>
2024-10-18 16:16:30 +0200

    fhrsdgsg

--- PIERREFEUILLESCIZZO/PIERREFEUILLESCIZZO.cpp
+++ PIERREFEUILLESCIZZO/PIERREFEUILLESCIZZO.cpp
@@ -6,22 +6,17 @@ int main()
 	const char* winP = "Pierre bat Ciseaux";
 	const char* annonce[] = { winC,winF,winP };
 	srand(time(0));
-
-	do	
+	int i = 200;
+	int num = ((rand() % 3) + 1);
+	while (i != num)
 	{
+		std::cout << num;
 		std::cout << "3: pierre 2: feuille 1: scissor " << std::endl;
-		int i;
 		std::cin >> i;
-		if (i == -1)
-			break;
-
-		int num = ((rand() % 4)-1);
-
-		std::cout << annonce[i - 1];
-		std::cout << "bravo";
-
-	} while (true);
-
+		int num = ((rand() % 3) + 1);
+	}
+	std::cout << annonce[i - 1];
+	std::cout << "bravo";
 	
 	return 0;
 }
